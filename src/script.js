572d1b19b262d3edd52fa265e76e0de4ad1d18fc
arutako <karikoji2525@gmail.com>
2025-10-30 21:27:09 +0900

    表のおおよそのロジックを作成した。 HTMLにGridとビンゴの数字を表示するようにした。

--- src/script.js
+++ src/script.js
@@ -1,15 +1,97 @@
+function checkNoUndefined(arr) {
+    if (arr.some(function (el) { return el === undefined; })) {
+        throw new Error("undefinedが出た!!!");
+    }
+}
 function shuffle(arr) {
-    var _a;
+    checkNoUndefined(arr);
     for (var i = arr.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
-        _a = [arr[j], arr[i]], arr[i] = _a[0], arr[j] = _a[1];
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     return arr;
 }
+/*
+ *ビンゴ表初期化
+ */
 var pool = Array.from({ length: 75 }, function (_, i) { return i + 1; });
 shuffle(pool);
+// console.log(pool);
 var grid = Array.from({ length: 5 }, function (_, r) {
     return Array.from({ length: 5 }, function (_, c) { return pool[r * 5 + c]; });
 });
 grid[2][2] = 0;
-console.table(grid);
+shuffle(pool);
+// console.table(grid);
+/*
+ *ビンゴ表表示
+ */
+function renderGrid(grid) {
+    // table開始タグ
+    var tableHTML = '<table border="1" cellspacing="0" cellpadding="4">';
+    for (var _i = 0, grid_1 = grid; _i < grid_1.length; _i++) {
+        var row = grid_1[_i];
+        tableHTML += '<tr>';
+        for (var _a = 0, row_1 = row; _a < row_1.length; _a++) {
+            var cell = row_1[_a];
+            tableHTML += "<td>".concat(cell, "</td>");
+        }
+        tableHTML += '</tr>';
+    }
+    tableHTML += '</table>';
+    var container = document.getElementById('grid-container');
+    if (container) {
+        container.innerHTML = tableHTML;
+    }
+}
+// grid配列を表示
+renderGrid(grid);
+/*
+ *ビンゴ判定
+ */
+function bingo() {
+    var first = pool.shift();
+    for (var i = 0; i < 5; i++) {
+        for (var j = 0; j < 5; j++) {
+            if (first === grid[i][j]) {
+                grid[i][j] = 0;
+            }
+        }
+    }
+    var element = document.getElementById('bingoNumber');
+    if (element) {
+        element.textContent = String(first);
+    }
+    var xZero = 0;
+    var yZero = 0;
+    var dZero = 0;
+    for (var i = 0; i < 5; i++) {
+        for (var j = 0; j < 5; j++) {
+            if (grid[i][j] === 0) {
+                xZero++;
+            }
+            else {
+                xZero = 0;
+            }
+            if (grid[j][i] === 0) {
+                yZero++;
+            }
+            else {
+                yZero = 0;
+            }
+            if ((grid[0][0] == 0 && grid[1][1] == 0 && grid[3][3] == 0 && grid[4][4] == 0) || (grid[0][4] == 0 && grid[1][3] == 0 && grid[3][1] == 0 && grid[4][0] == 0)) {
+                dZero = 1;
+            }
+            if ((xZero === 5) || (yZero === 5) || (dZero === 1)) {
+                var bingoCheck = document.getElementById('bingo');
+                if (bingoCheck) {
+                    bingoCheck.textContent = String("BINGO!!!");
+                }
+            }
+        }
+    }
+    console.table(grid);
+    renderGrid(grid);
+}
--- src/script.ts
+++ src/script.ts
@@ -1,18 +1,108 @@
-function shuffle<T>(arr: T[]): T[] {
+function checkNoUndefined<T>(arr: (T | undefined)[]): asserts arr is T[] {
+    if(arr.some(el => el === undefined)) {
+        throw new Error("undefinedが出た!!!");
+    }
+}
+
+
+function shuffle(arr: number[]): number[] {
+    checkNoUndefined(arr);
+
     for(let i = arr.length -1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [arr[i]!, arr[j]!] = [arr[j]!, arr[i]!];
-    } 
+        const temp = arr[i];
+        arr[i] = arr[j] as number;
+        arr[j] = temp as number;
+    }
     return arr;
 }
 
+/*
+ *ビンゴ表初期化
+ */
+
 const pool = Array.from({ length: 75 }, (_, i) => i + 1);
 shuffle(pool);
+// console.log(pool);
 
-const grid: number[][] = Array.from({ length: 5 }, (_, r) =>
-    Array.from({ length: 5 }, (_, c) => pool[r * 5 + c]!)
+const grid = Array.from({ length: 5 }, (_, r) =>
+    Array.from({ length: 5 }, (_, c) => pool[r * 5 + c])
 )
 
 grid[2]![2] = 0;
+shuffle(pool);
+// console.table(grid);
+/*
+ *ビンゴ表表示
+ */
+ function renderGrid(grid: (number | undefined)[][]) {
+    // table開始タグ
+    let tableHTML = '<table border="1" cellspacing="0" cellpadding="4">';
+    for (const row of grid) {
+      tableHTML += '<tr>';
+      for (const cell of row) {
+        tableHTML += `<td>${cell}</td>`;
+      }
+      tableHTML += '</tr>';
+    }
+    tableHTML += '</table>';
+  
+    const container = document.getElementById('grid-container');
+    if (container) {
+      container.innerHTML = tableHTML;
+    }
+  }
+  
+  // grid配列を表示
+  renderGrid(grid);
 
-console.table(grid);
+/*
+ *ビンゴ判定
+ */
+ function bingo() {
+
+    let first = pool.shift();
+    for (let i = 0; i < 5; i++){
+        for (let j = 0; j < 5; j++){
+            if(first === grid[i]![j]){
+                grid[i]![j] = 0;
+            }
+        }
+    }
+    const element = document.getElementById('bingoNumber');
+    if (element) {
+        element.textContent = String(first);
+    }
+
+    let xZero = 0;
+    let yZero = 0;
+    let dZero = 0;
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+            if(grid[i]![j] === 0) {
+                xZero++;
+            }
+            else {
+                xZero = 0;
+            }
+            if(grid[j]![i] === 0) {
+                yZero++;
+            }
+            else {
+                yZero = 0;
+            }
+            if((grid[0]![0] == 0 && grid[1]![1] == 0 && grid[3]![3] == 0 && grid[4]![4] == 0) || (grid[0]![4] == 0 && grid[1]![3] == 0 && grid[3]![1] == 0 && grid[4]![0] == 0)) {
+                dZero = 1;
+            }
+
+            if((xZero === 5) || (yZero === 5) || (dZero === 1)) {
+                const bingoCheck = document.getElementById('bingo');
+                if (bingoCheck) {
+                    bingoCheck.textContent = String("BINGO!!!");
+                }
+            }
+        }
+    }
+    console.table(grid);
+    renderGrid(grid);
+ }
